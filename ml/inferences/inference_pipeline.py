b1b03ca3c74cf2f8812eb6092716ba48a03351ae
Your Name <you@example.com>
2023-11-10 19:16:58 +0300

    pipelines modify: ends with grad

--- ml/inferences/inference_pipeline.py
+++ ml/inferences/inference_pipeline.py
@@ -9,7 +9,7 @@ class InferencePipeline:
         self.model_folder = model_folder
         self.data_folder = data_folder
 
-    def inference_pipeline_consume(self, image_data):
+    def consume(self, image_data):
         # hd_result - person bbox and poses coords, list of dicts
         hd_result:list = HumanDetection().predict(image_data)
         bbox_coord, persons_list = people_selector(image_data, hd_result)
@@ -19,10 +19,18 @@ class InferencePipeline:
             wd_result = WeaponDetection().predict(persons_list[_id])
             # dict
             wpd_result = WeaponPoseDetection().predict(persons_list[_id])
-            result.append(
-                dict(id=_id, human_bbox_coord=bbox_coord[_id],
-                wd_bbox_ccord=wd_result["bbox"], wpd_result=wpd_result) # FIXME: отладь wpd_result
-            )
-        # TODO: later
+            # TODO: fix result
+            if wd_result or wpd_result:
+                result.append(
+                    dict(
+                        id=_id,
+                        human_bbox_coord=bbox_coord[_id], wpd_result=wpd_result,
+                        wd_bbox_ccord=wd_result["bbox"], wd_result=wd_result
+                    ) # FIXME: отладь wpd_result
+                )
+        # TODO: later, after MVP
         # = people_jointer(image_data, )
-        return result
+        if result is []:
+            return False, []
+        else:
+            return True, result
